41b0365fc8c14e1543dfa8acd22748f08e27a0d0
Ivo Geersen <ivo@ypho.nl>
2022-01-05 08:44:41 +0100

    Changed 'exclude' to 'only' in getColorsByShade()

--- README.md
+++ README.md
@@ -1,33 +1,39 @@
 # Colors
+
 ![License](https://img.shields.io/github/license/ypho/colors)
 ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/ypho/colors/Installation%20&%20Tests)
 ![Code Size](https://img.shields.io/github/languages/code-size/ypho/colors)
+![Latest Release](https://img.shields.io/github/v/release/ypho/colors)
 
-Colors is a small package that contains 22 color palettes. Each palette has ten shades from light to dark. The colors are based on the [TailwindCSS](https://tailwindcss.com/docs/customizing-colors#default-color-palette) colors. 
+Colors is a small package that contains 22 color palettes. Each palette has ten shades from light to dark. The colors
+are based on the [TailwindCSS](https://tailwindcss.com/docs/customizing-colors#default-color-palette) colors.
 
 ## Installation
+
 ```bash
 composer require ypho/colors
 ```
 
 ## Usage
+
 ```php
-// Return given color, default ShadeIndex 3
-$color = Color::getSingleColor('red'); // #fee2e2
+// Return given color
+$regularRed = Color::getSingleColor('red'); // #ef4444
 
 // Return given color, but a darker shade of red
-$color = Color::getSingleColor('red', 7); // #b91c1c
+$amazonGreen = Color::getSingleColor('green', 8); // #166534
 
 // Returns a palette of 10 shades of the given color
-$palette = Color::getColorPalette('teal'); // ['#f0fdfa' ... '#134e4a']
+$teal = Color::getColorPalette('teal'); // ['#f0fdfa' ... '#134e4a']
 
-// Returns all colors, but only ShadeIndex = 2 (light)
+// Returns all colors, in a light shade
 $lightColors = Color::getColorsByShade(2); // ['#e2e8f0' ... '#fecdd3']
 
-// Same as above, only ShadeIndex = 8 (darker shade), and exclude red
-$darkColorsExceptRed = Color::getColorsByShade(8, ['red']);
+// Same as above, but take only three colors
+$trafficLight = Color::getColorsByShade(6, ['red', 'orange', 'green']); // ['#dc2626', '#f97316', '#22c55e']
 ```
 
 ## Donations
+
 ![BTC](https://img.shields.io/badge/BTC-bc1qaf590wa6gvgzhq60pwez9jp2nup643laasj7yl-blue)
 ![ETH](https://img.shields.io/badge/ETH-0x4B5cA9188A55d2CEb6E7765B7f23EceF3530531B-red)
--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -5,17 +5,33 @@ use Ypho\Colors\Color;
 
 class ColorTest extends TestCase
 {
+    public function test_colors() {
+        $this->assertIsArray(Color::COLORS);
+
+        // Check if each color has 10 Shades
+        foreach(Color::COLORS as $color) {
+            $this->assertIsArray($color);
+            $this->assertCount(10, $color);
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
     public function test_getSingleColor()
     {
         $this->assertEquals('#dbeafe', Color::getSingleColor('blue', 1));
-        $this->assertEquals('#6366f1', Color::getSingleColor('indigo', 5));
-        $this->assertEquals('#bef264', Color::getSingleColor('lime'));
+        $this->assertEquals('#6366f1', Color::getSingleColor('indigo'));
+        $this->assertEquals('#84cc16', Color::getSingleColor('lime'));
 
         $this->expectExceptionMessage('Given Palette and/or Shade not found!');
         Color::getSingleColor('nonExisting');
         Color::getSingleColor('green', 12345);
     }
 
+    /**
+     * @throws Exception
+     */
     public function test_getColorPalette()
     {
         $this->assertIsArray(Color::getColorPalette('yellow'));
@@ -27,15 +43,21 @@ class ColorTest extends TestCase
         $this->assertIsArray(Color::getColorPalette('NonExistingColor'));
     }
 
+    /**
+     * @throws Exception
+     */
     public function test_getColorsByShade()
     {
         $this->assertIsArray(Color::getColorsByShade());
         $this->assertIsArray(Color::getColorsByShade(5, ['red', 'green', 'blue']));
 
         $this->assertCount(22, Color::getColorsByShade(8));
-        $this->assertCount(20, Color::getColorsByShade(4, ['orange', 'slate']));
+        $this->assertCount(3, Color::getColorsByShade(4, ['orange', 'red', 'yellow']));
 
-        $this->expectExceptionMessage('Given shade not found, choose between 0 and 9!');
+        $this->expectExceptionMessage('Given Shade not found, choose between 0 and 9!');
         $this->assertIsArray(Color::getColorsByShade(123));
+
+        $this->expectExceptionMessage('Given Palette and/or Shade not found!');
+        $this->assertIsArray(Color::getColorsByShade(1, ['NonExistingColor']));
     }
 }
